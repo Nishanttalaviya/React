9b8664c3a130d1fdf53f0e5e7461f1a6cd646b02
nishant talaviya <nishanttalaviya2308@gmail.com>
2025-03-10 08:13:31 +0530

    update code

---
+++ Project2/To-Do/src/config/db.config.js
@@ -0,0 +1,11 @@
+const MYSQL_DB_CONFIG = {
+    HOST: "localhost",
+    USER: "root",
+    PORT: 3306,
+    PASSWORD: "",
+    DB: "my_ecom_rku_db",
+  };
+  module.exports = {
+    MYSQL_DB_CONFIG,
+  };
+  
